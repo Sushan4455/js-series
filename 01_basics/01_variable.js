1b93c337c0c720ba5fcdd35b3b9307619588e082
Sushan Karki <147791962+Sushan4455@users.noreply.github.com>
2025-09-13 10:19:31 +0000

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let  accountEmail = "sushan@google.com"
+var accountPassword = "12345"
+accountCity = "Bhaktapur"
+
+accountEmail ="hc@hc.com"
+accountPassword = "212123212"
+accountCity = "Sindhuli"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
